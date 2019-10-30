41900602a73cb0d101b37169a09a57dcde561ed4
Evgenii Efremov <johne32rus23@gmail.com>
2019-10-30 06:43:01 +0300

    first acceptanse test passed

--- Gemfile
+++ Gemfile
@@ -58,6 +58,8 @@ group :test do
   gem 'rails-controller-testing'
   gem 'shoulda-matchers'
   gem 'webdrivers'
+  # gem for save_and_open_page
+  gem 'launchy'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -7,10 +7,10 @@ feature 'User can sign in', "
   scenario 'Registred user tries to sign in' do
     User.create!(email: 'user@test.com', password: 'password')
 
-    visit '/login'
+    visit new_user_session_path
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: 'password'
-
+    click_on 'Log in'
     expect(page).to have_content 'Signed in successfully.'
   end
   scenario 'Unregistred user tries to sign in'
