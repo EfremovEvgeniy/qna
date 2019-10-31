cedafd4c46e0815e130dff788c4c53dcee246c99
Evgenii Efremov <johne32rus23@gmail.com>
2019-10-31 07:44:42 +0300

    user can see list of questions

--- spec/features/questions/index_spec.rb
+++ spec/features/questions/index_spec.rb
@@ -1,20 +1,35 @@
 require 'rails_helper'
 
 feature 'User can see list of  questions', "
-  In order to acquainted with questions 
+  In order to acquainted with questions
   As an authenticated or unauthenticated user
   I'd like to be able to see list of questions
 " do
-  
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
 
   scenario 'Unauthenticated user can see list of questions' do
     visit root_path
-  
+
     expect(page).to have_content 'All questions'
-    expect(page).to have_content question.title
-    expect(page).to have_content question.body
+    expect(page).to have_css('td#question_title', text: question.title)
+    expect(page).to have_css('td#question_body', text: question.body)
+    expect(page).to have_css('td#question_title', count: Question.all.count)
   end
 
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
+      visit root_path
+    end
+
+    scenario 'can see list of questions' do
+      visit root_path
+
+      expect(page).to have_content 'All questions'
+      expect(page).to have_css('td#question_title', text: question.title)
+      expect(page).to have_css('td#question_body', text: question.body)
+      expect(page).to have_css('td#question_title', count: Question.all.count)
+    end
+  end
 end
