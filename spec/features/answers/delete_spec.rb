b2f8a2b82b02ce37ccaae9e0a4d7a03b00053be7
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-03 11:07:11 +0300

    add tests for delete answer

--- spec/features/answers/delete_spec.rb
+++ spec/features/answers/delete_spec.rb
@@ -6,6 +6,7 @@ feature 'User can delete only his answer', "
 " do
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
+  given!(:question_with_answer) { create(:question_with_answer) }
 
   describe 'Authenticated user' do
     background do
@@ -20,5 +21,16 @@ feature 'User can delete only his answer', "
       click_on 'delete'
       expect(page).to have_no_link('delete')
     end
+
+    scenario 'tries to delete not his own answer' do
+      visit question_path(question_with_answer)
+      expect(page).to have_no_link('delete')
+    end
+  end
+
+  scenario 'Unauthenticated user tries to delete answer' do
+    visit question_path(question_with_answer)
+    expect(page).to have_content 'You need to sign in to write your answer'
+    expect(page).to have_no_link('delete')
   end
 end
