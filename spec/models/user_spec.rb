c2fb737439708b214c67d5f49bccd709eaf74356
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-03 10:34:53 +0300

    add tests for author_of method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe User, type: :model do
 
   it { should have_many(:questions).dependent(:destroy) }
   it { should have_many(:answers).dependent(:destroy) }
+
+  describe 'author_of?(resource) method' do
+    let(:first_user) { create(:user) }
+    let(:second_user) { create(:user) }
+    let(:question) { create(:question, user: first_user) }
+
+    it 'should confirm that first_user is an author of the passed resource' do
+      expect(first_user).to be_author_of(question)
+    end
+
+    it 'should deny that second_user is an author of the passed resource' do
+      expect(second_user).to_not be_author_of(question)
+    end
+  end
 end
