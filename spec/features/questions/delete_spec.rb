9a18b041860fe8db92c1738a47ae0324912fd395
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-03 11:30:50 +0300

    fix tests for delete questions

--- spec/features/questions/delete_spec.rb
+++ spec/features/questions/delete_spec.rb
@@ -4,21 +4,25 @@ feature 'User can delete only his question', "
   As an authenticated user
   I'd like to be able to delete only my question
 " do
-  given(:user) { create(:user_with_question) }
   given(:question) { create(:question) }
 
   describe 'Authenticated user' do
     background do
-      sign_in(user)
+      sign_in(question.user)
       visit root_path
     end
 
     scenario 'delete his question' do
-      expect(page).to have_content user.questions.first.title
       expect(page).to have_content question.title
       expect(page).to have_link('delete')
       click_on 'delete'
+      expect(page).to have_no_content question.title
       expect(page).to have_no_link('delete')
     end
+  end
+
+  scenario 'unauthenticated user tries to delete question' do
+    visit root_path
+    expect(page).to have_no_link('delete')
   end
 end
