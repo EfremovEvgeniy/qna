3c4b49f62e1e7bbbd27e1c47fe15e714f940a557
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-07 06:27:33 +0300

    refactor answer form

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -21,6 +21,10 @@ RSpec.describe QuestionsController, type: :controller do
     before { login_with(user) }
     before { get :show, params: { id: question } }
 
+    it 'assigns new answer for question' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+    
     it 'renders show view' do
       expect(response).to render_template :show
     end
