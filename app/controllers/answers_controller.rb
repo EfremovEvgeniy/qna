2a097981a8a55c75de75414e7c3241d594edf867
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-07 07:23:14 +0300

    add some fixes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,17 +5,18 @@ class AnswersController < ApplicationController
   def new; end
 
   def create
-    @answer = question.answers.build(answer_params)
-    @answer.user = current_user
-    if @answer.save
+    @new_answer = question.answers.build(answer_params)
+    @new_answer.user = current_user
+    if @new_answer.save
       redirect_to @question, notice: 'Your answer successfully created.'
     else
+      # byebug
       render 'questions/show'
     end
   end
 
   def destroy
-    answer.destroy if current_user.author_of?(answer)
+    @answer.destroy if current_user.author_of?(answer)
     redirect_to @answer.question, notice: 'Your answer successfully deleted.'
   end
 
