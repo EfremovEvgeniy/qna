3e1c0a53d4806d702da72b338c64d77b6a2f1237
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-07 08:19:24 +0300

    add test for create answer with erorrs

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe QuestionsController, type: :controller do
     it 'assigns new answer for question' do
       expect(assigns(:answer)).to be_a_new(Answer)
     end
-    
+
     it 'renders show view' do
       expect(response).to render_template :show
     end
--- spec/features/answers/create_spec.rb
+++ spec/features/answers/create_spec.rb
@@ -8,26 +8,35 @@ feature 'User on the question page can write answer', "
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
 
-  describe 'Authenticated user' do
+  describe 'Authenticated user', js: true do
     background do
       sign_in(user)
+      visit question_path(question)
     end
 
     scenario 'can write his answer' do
-      visit question_path(question)
       expect(page).to have_field('Body')
       expect(page).to have_no_content('You need to sign in to write your answer')
+
       fill_in 'Body', with: 'my awesome answer'
       click_on 'Save'
+
       expect(page).to have_content 'Your answer successfully created.'
       within '.answers' do
         expect(page).to have_content 'my awesome answer'
       end
     end
+
+    scenario 'creates answer with errors' do
+      click_on 'Save'
+
+      expect(page).to have_content "Body can't be blank"
+    end
   end
 
   scenario 'Unauthenticated user tries to ask a question' do
     visit question_path(question)
+
     expect(page).to have_content 'You need to sign in to write your answer'
   end
 end
