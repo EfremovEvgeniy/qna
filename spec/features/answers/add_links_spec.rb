c6138a80b1ce522aa055f776ee6e617a003d8468
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-15 11:51:51 +0300

    user can add links to a

--- spec/features/answers/add_links_spec.rb
+++ spec/features/answers/add_links_spec.rb
@@ -7,17 +7,37 @@ feature 'User can add links to answer', "
 " do
   given!(:question) { create(:question) }
   given(:gist_url) { 'https://gist.github.com/EfremovEvgeniy/79dcd2444231b4269f06068fcd143fd0' }
+  given(:gist_url_2) { 'https://gist.github.com/EfremovEvgeniy/f4ac4b60a3ab5afe34ce70c578143a9c' }
 
-  scenario 'User adds link when asks question', js: true do
-    sign_in(question.user)
-    visit question_path(question)
-    fill_in 'Body', with: 'my awesome answer'
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
-    click_on 'Create'
+  describe 'User adds', js: true do
+    background do
+      sign_in(question.user)
+      visit question_path(question)
+      fill_in 'Body', with: 'my awesome answer'
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
+    end
 
-    within '.answers' do
-      expect(page).to have_link 'My gist', href: gist_url
+    scenario 'link when answer question' do
+      click_on 'Create'
+
+      within '.answers' do
+        expect(page).to have_link 'My gist', href: gist_url
+      end
+    end
+
+    scenario 'links when answer question' do
+      click_on 'add more'
+      within all('.nested-fields')[1] do
+        fill_in 'Link name', with: 'My gist 2'
+        fill_in 'Url', with: gist_url_2
+      end
+      click_on 'Create'
+
+      within '.answers' do
+        expect(page).to have_link 'My gist', href: gist_url
+        expect(page).to have_link 'My gist 2', href: gist_url_2
+      end
     end
   end
 end
