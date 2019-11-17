e7f3e453661b4935992b7ae9f02c2bbc7bf42a87
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-17 16:10:05 +0300

    add concern and shared example

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,12 @@
 class Question < ApplicationRecord
+  include LinksAssociations
+
   has_many :answers, dependent: :destroy
-  has_many :links, dependent: :destroy, as: :linkable
   has_one :trophy, dependent: :destroy
   belongs_to :user
 
   has_many_attached :files
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :trophy, reject_if: :all_blank, allow_destroy: true
 
   validates :title, :body, presence: true
