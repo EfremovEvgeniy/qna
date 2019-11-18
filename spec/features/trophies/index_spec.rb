3ff8f90ba6505e2d44cf8f660a5c77d1330ad589
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-18 11:57:46 +0300

    add test for trophy

--- spec/features/trophies/index_spec.rb
+++ spec/features/trophies/index_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 feature 'User can view a list of badges' do
   given(:user) { create(:user) }
+  given(:second_user) { create(:user) }
   given(:question) { create(:question) }
   given(:second_question) { create(:question) }
   given!(:trophy) { create(:trophy, question: question, user: user) }
@@ -10,14 +11,21 @@ feature 'User can view a list of badges' do
   describe 'Authenticated user' do
     background do
       sign_in(user)
-      click_on 'My trophies'
     end
 
     scenario 'see list of trophies' do
+      click_on 'My trophies'
       expect(page).to have_content trophy.name
       expect(page).to have_content trophy.question.title
       expect(page).to have_content second_trophy.name
       expect(page).to have_content second_trophy.question.title
+    end
+
+    scenario 'can not see not his trophy' do
+      second_trophy.update(user: second_user)
+      click_on 'My trophies'
+
+      expect(page).to have_no_content second_trophy.name
     end
   end
 
