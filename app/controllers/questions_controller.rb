db5bcaef074d57ae8c0809db6952490d319c2e45
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-22 09:45:05 +0300

    commit after screencast

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@ class QuestionsController < ApplicationController
 
   before_action :authenticate_user!, except: %i[index show]
   helper_method :question
+  after_action :publish_question, only: :create 
 
   def index
     @questions = Question.all
@@ -39,6 +40,17 @@ class QuestionsController < ApplicationController
   end
 
   private
+
+  def publish_question
+    return if @question.errors.any?
+    ActionCable.server.broadcast(
+      'questions_channel',
+      ApplicationController.render(
+        partial: 'questions/simple_question',
+        locals: { question: @question }
+        )
+      )
+  end
 
   def question
     @question ||= params[:id] ? Question.with_attached_files.find(params[:id]) : Question.new
