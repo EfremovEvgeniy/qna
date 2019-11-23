e82f36a17685cc289e7a67e47688faadd7844417
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-23 10:04:48 +0300

    add test for questions channel

--- spec/channels/questions_channel_spec.rb
+++ spec/channels/questions_channel_spec.rb
@@ -6,27 +6,46 @@ feature 'User can see in real time appearance of new questions', "
 " do
   given(:user) { create(:user) }
 
-  scenario 'question appears on another user page', js: true do
-    in_browser(:one) do
-      sign_in(user)
-      visit questions_path
+  describe 'new question', js: true do
+    background do
+      in_browser(:one) do
+        sign_in(user)
+        visit questions_path
+      end
+
+      in_browser(:two) do
+        visit questions_path
+      end
     end
 
-    in_browser(:two) do
-      visit questions_path
+    scenario 'question appears on another user page' do
+      in_browser(:one) do
+        click_on 'Ask question'
+        fill_in 'Title', with: 'Test question'
+        fill_in 'Body', with: 'text text text'
+        click_on 'Ask'
+
+        expect(page).to have_content 'Test question'
+      end
+
+      in_browser(:two) do
+        expect(page).to have_content 'Test question'
+      end
     end
 
-    in_browser(:one) do
-      click_on 'Ask question'
-      fill_in 'Title', with: 'Test question'
-      fill_in 'Body', with: 'text text text'
-      click_on 'Ask'
+    scenario 'question with errors does not appear on another user page' do
+      in_browser(:one) do
+        click_on 'Ask question'
+        fill_in 'Title', with: 'Test question'
+        click_on 'Ask'
 
-      expect(page).to have_content 'Test question'
-    end
+        expect(page).to have_content "Body can't be blank"
+        expect(page).to have_no_content 'Test question'
+      end
 
-    in_browser(:two) do
-      expect(page).to have_content 'Test question'
+      in_browser(:two) do
+        expect(page).to have_no_content 'Test question'
+      end
     end
   end
 end
