eca8b6bfe0ef890c589319572c036c9c98d8c268
Evgenii Efremov <johne32rus23@gmail.com>
2019-11-25 19:47:20 +0300

    add test for answer channel

--- app/javascript/channels/answers_channel.js
+++ app/javascript/channels/answers_channel.js
@@ -1,9 +1,7 @@
 import consumer from "./consumer"
 
-
 consumer.subscriptions.create("AnswersChannel", {
   connected() {
-    console.log('Connected');
     if (gon.question_id) {
       this.perform('follow', {
         id: gon.question_id
@@ -18,7 +16,7 @@ consumer.subscriptions.create("AnswersChannel", {
   received(data) {
     var answer = JSON.parse(data)
     if (gon.user_id !== answer.user_id) {
-      $('.answers').append(JST['templates/answer']({answer: answer}))
+      // передать answer в js template
     }
   }
 });
--- spec/channels/answers_channel_spec.rb
+++ spec/channels/answers_channel_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe AnswersChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+feature 'User can see in real time appearance of new answers', "
+  As an user
+  I'd like to be able see appearance of new users answers
+" do
+  given(:question) { create(:question) }
+
+  describe 'new answer', js: true do
+    background do
+      in_browser(:one) do
+        sign_in(question.user)
+        visit question_path(question)
+      end
+
+      in_browser(:two) do
+        visit question_path(question)
+      end
+    end
+
+    scenario 'appears on another user page' do
+      in_browser(:one) do
+        fill_in 'Body', with: 'my awesome answer'
+        click_on 'Create'
+
+        within '.answers' do
+          expect(page).to have_content 'my awesome answer'
+        end
+      end
+
+      in_browser(:two) do
+        expect(page).to have_content 'my awesome answer'
+      end
+    end
+
+    scenario 'with errors does not appear on another user page' do
+      in_browser(:one) do
+        click_on 'Create'
+
+        expect(page).to have_content "Body can't be blank"
+      end
+
+      in_browser(:two) do
+        within '.answers' do
+          expect(page).to have_no_css 'answer'
+        end
+      end
+    end
+  end
 end
