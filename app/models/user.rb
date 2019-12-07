be217fd30c08a08de3d78c7754b5a7d0d4c57133
Evgenii Efremov <johne32rus23@gmail.com>
2019-12-07 17:57:34 +0300

    realize find_for_oauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,27 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :authorizations, dependent: :destroy
 
-  def self.find_for_oauth(params); end
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+    end
+    user.create_authorization(auth)
+
+    user
+  end
 
   def author_of?(resource)
     id == resource.user_id
+  end
+
+  def create_authorization(auth)
+    authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
