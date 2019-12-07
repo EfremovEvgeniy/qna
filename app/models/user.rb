30e629ee50e76b1e7ba132a172af1b044790e50d
Evgenii Efremov <johne32rus23@gmail.com>
2019-12-07 17:01:51 +0300

    add authorization model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :trophies, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
 
   def self.find_for_oauth(params); end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,25 @@ RSpec.describe User, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
   it { should have_many(:trophies).dependent(:destroy) }
   it { should have_many(:votes).dependent(:destroy) }
+  it { should have_many(:authorizations).dependent(:destroy) }
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  describe 'author_of?(resource) method' do
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123') }
+
+    context 'user already has authorization' do
+      it 'returns the user' do
+        user.authorizations.create(provider: 'facebook', uid: '123')
+
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+  end
+
+  describe '#author_of?(resource)' do
     let(:first_user) { create(:user) }
     let(:second_user) { create(:user) }
     let(:question) { create(:question, user: first_user) }
