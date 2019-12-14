010427b1dbcde9ad324ba2e6614e8ae233479af5
Evgenii Efremov <johne32rus23@gmail.com>
2019-12-14 13:14:33 +0300

    refactored ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,23 +14,33 @@ class Ability
   end
 
   def user_abilities
-    guest_abilities
+    can :read, [Question, Trophy]
 
-    can :create, [Question, Answer]
+    can :create, [Question, Answer, Comment]
 
-    can %i[update destroy], [Question, Answer], user_id: user.id
+    can %i[update destroy], [Question, Answer] do |resource|
+      user.author_of?(resource)
+    end
 
-    can %i[vote_up vote_down], [Question, Answer]
-    cannot %i[vote_up vote_down], [Question, Answer], user_id: user.id
+    can %i[vote_up vote_down], [Question, Answer] do |votable|
+      !user.author_of?(votable)
+    end
 
-    can :make_best, Answer
-    cannot :make_best, Answer, user_id: user.id
+    can :destroy, ActiveStorage::Attachment do |att|
+      user.author_of?(att)
+    end
 
-    can :make_best, Question, user_id: user.id
+    can :destroy, Link do |link|
+      user.author_of?(link.linkable)
+    end
 
-    can :create, Comment
+    can :make_best, Answer do |answer|
+      !user.author_of?(answer)
+    end
 
-    can :read, Trophy
+    can :make_best, Question do |question|
+      user.author_of?(question)
+    end
   end
 
   def guest_abilities
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -18,33 +18,21 @@ RSpec.describe Ability do
       it { should be_able_to :create, Question }
       it { should be_able_to :read, Question }
 
-      it { should be_able_to :update, create(:question, user_id: user.id) }
-      it { should_not be_able_to :update, create(:question, user_id: second_user.id) }
+      it { should be_able_to %i[update destroy], create(:question, user_id: user.id) }
+      it { should_not be_able_to %i[update destroy], create(:question, user_id: second_user.id) }
 
-      it { should be_able_to :destroy, create(:question, user_id: user.id) }
-      it { should_not be_able_to :destroy, create(:question, user_id: second_user.id) }
-
-      it { should be_able_to :vote_up, create(:question, user_id: second_user.id) }
-      it { should_not be_able_to :vote_up, create(:question, user_id: user.id) }
-
-      it { should be_able_to :vote_down, create(:question, user_id: second_user.id) }
-      it { should_not be_able_to :vote_down, create(:question, user_id: user.id) }
+      it { should be_able_to %i[vote_up vote_down], create(:question, user_id: second_user.id) }
+      it { should_not be_able_to %i[vote_up vote_down], create(:question, user_id: user.id) }
     end
 
     describe 'answers' do
       it { should be_able_to :create, Answer }
 
-      it { should be_able_to :update, create(:answer, user_id: user.id) }
-      it { should_not be_able_to :update, create(:answer, user_id: second_user.id) }
+      it { should be_able_to %i[update destroy], create(:answer, user_id: user.id) }
+      it { should_not be_able_to %i[destroy destroy], create(:answer, user_id: second_user.id) }
 
-      it { should be_able_to :destroy, create(:answer, user_id: user.id) }
-      it { should_not be_able_to :destroy, create(:answer, user_id: second_user.id) }
-
-      it { should be_able_to :vote_up, create(:answer, user_id: second_user.id) }
-      it { should_not be_able_to :vote_up, create(:answer, user_id: user.id) }
-
-      it { should be_able_to :vote_down, create(:answer, user_id: second_user.id) }
-      it { should_not be_able_to :vote_down, create(:answer, user_id: user.id) }
+      it { should be_able_to %i[vote_up vote_down], create(:answer, user_id: second_user.id) }
+      it { should_not be_able_to %i[vote_up vote_down], create(:answer, user_id: user.id) }
 
       it { should be_able_to :make_best, create(:answer, user_id: second_user.id) }
       it { should_not be_able_to :make_best, create(:answer, user_id: user.id) }
@@ -56,6 +44,17 @@ RSpec.describe Ability do
 
     describe 'trophies' do
       it { should be_able_to :read, Trophy }
+    end
+
+    describe 'links' do
+      let(:question) { create(:question, user: user) }
+      let(:second_question) { create(:question) }
+      it { should be_able_to :destroy, create(:link, linkable: question) }
+      it { should_not be_able_to :destroy, create(:link, linkable: second_question) }
+    end
+
+    describe 'attachments' do
+      it { should be_able_to :destroy, ActiveStorage::Attachment }
     end
   end
 end
