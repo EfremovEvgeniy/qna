4c297df86fbfa48bd31107b4d9876545e64ca0d8
Evgenii Efremov <johne32rus23@gmail.com>
2020-01-09 09:11:21 +0300

    add tests for sidekiq web

--- spec/features/sidekiq_web_ability/sidekiq_web_ability_spec.rb
+++ spec/features/sidekiq_web_ability/sidekiq_web_ability_spec.rb
@@ -2,14 +2,27 @@ require 'rails_helper'
 
 feature 'User with admin email can see sidekiq web' do
   given(:user) { create(:user) }
-  given(:admin) { create(:user, email: 'johne32rus23@gmail.com') }
+  given(:admin) { create(:user, email: ENV['ADMIN_EMAIL']) }
 
   describe 'Authenticated user' do
-    scenario 'with admin email'
-    scenario 'with not admin email'
+    scenario 'with admin email' do
+      sign_in(admin)
+      visit sidekiq_web_path
+      expect(page).to have_content 'Dashboard'
+    end
+
+    scenario 'with not admin email' do
+      sign_in(user)
+      expect do
+        visit sidekiq_web_path
+      end.to raise_error(ActionController::RoutingError)
+    end
   end
 
   describe 'Unauthenticated user' do
-    scenario 'can not see sidekiq web'
+    scenario 'can not see sidekiq web' do
+      visit sidekiq_web_path
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
   end
 end
