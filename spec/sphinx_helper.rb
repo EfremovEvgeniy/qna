dc7f0e134eaf8cec36230c93bf8e1682384fd71a
Evgenii Efremov <johne32rus23@gmail.com>
2020-01-13 18:01:53 +0300

    confidure and add feature tests

--- spec/sphinx_helper.rb
+++ spec/sphinx_helper.rb
@@ -6,7 +6,7 @@ RSpec.configure do |config|
   # DatabaseCleaner settings
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-    # Ensure searches directories exist for the test environment
+    # Ensure sphinx directories exist for the test environment
     ThinkingSphinx::Test.init
     # Configure and start Sphinx, and automatically stop Sphinx at the end of the test suite.
     ThinkingSphinx::Test.start_with_autostop
@@ -16,7 +16,7 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, searches: true) do
+  config.before(:each, sphinx: true) do
     DatabaseCleaner.strategy = :truncation
     # Index data when running an acceptance spec.
     ThinkingSphinx::Test.index
